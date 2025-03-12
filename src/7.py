fe8b6ab0a196c8a3345ebb37bca55781714e664e
cailkosik658 <cailkosik658@gmail.com>
2025-03-12 15:32:28 +0000

    Add 7.py

---
+++ src/7.py
@@ -0,0 +1,7 @@
+import random
+
+def get_random_code(length=10):
+    chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
+    return ''.join(random.choice(chars) for _ in range(length))
+
+print(get_random_code())
