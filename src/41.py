bd4cb88c049afaaaef08ef473f57ea01bb9e1e99
cailkosik658 <cailkosik658@gmail.com>
2025-05-26 17:46:04 +0000

    Add 41.py

---
+++ src/41.py
@@ -0,0 +1,14 @@
+def greet_user(name):
+    """
+    This function greets a user with their name.
+    
+    Parameters:
+    - name: The name of the user to greet.
+    Returns:
+    - A message greeting the user.
+    """
+    return f"Hello, {name}!"
+
+# Example usage
+user_name = "Alice"
+print(greet_user(user_name))
